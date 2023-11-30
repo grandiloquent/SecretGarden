cf51dce9ea58af1ff44be5e8a4fd461a335cefe0
grandiloquent <1785907865@qq.com>
2023-11-30 23:06:34 +0800

    优化播放器

--- app/src/main/java/psycho/euphoria/v/PlayerActivity.java
+++ app/src/main/java/psycho/euphoria/v/PlayerActivity.java
@@ -1,73 +1,48 @@
 package psycho.euphoria.v;
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
-import android.content.res.Configuration;
-import android.graphics.Matrix;
-import android.graphics.SurfaceTexture;
-import android.graphics.Typeface;
-import android.media.MediaPlayer;
-import android.media.PlaybackParams;
-import android.media.TimedMetaData;
-import android.opengl.GLES20;
-import android.os.Build.VERSION;
-import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
-import android.os.Environment;
-import android.os.Handler;
-import android.os.SystemClock;
-import android.preference.PreferenceManager;
-import android.util.Log;
-import android.view.GestureDetector;
-import android.view.GestureDetector.SimpleOnGestureListener;
-import android.view.MotionEvent;
-import android.view.ScaleGestureDetector;
-import android.view.ScaleGestureDetector.SimpleOnScaleGestureListener;
-import android.view.Surface;
-import android.view.TextureView;
-import android.view.TextureView.SurfaceTextureListener;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.View.OnTouchListener;
-import android.view.ViewConfiguration;
-import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.FrameLayout;
-import android.widget.FrameLayout.LayoutParams;
-import android.widget.ImageButton;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Formatter;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-import javax.microedition.khronos.egl.EGL10;
-import javax.microedition.khronos.egl.EGLConfig;
-import javax.microedition.khronos.egl.EGLContext;
-import javax.microedition.khronos.egl.EGLDisplay;
-import javax.microedition.khronos.egl.EGLSurface;
-
-import psycho.euphoria.v.TimeBar.OnScrubListener;
-
-import static psycho.euphoria.v.PlayerUtils.hideSystemUI;
-import static psycho.euphoria.v.Shared.getStringForTime;
 
 public class PlayerActivity extends Activity {
 
     public static final String KEY_VIDEO_FILE = "VideoFile";
     public static final String KEY_VIDEO_TITLE = "VideoTitle";
 
+    private VideoView mVideoView;
+
+    public static void launchActivity(Context context, File file, int sort) {
+        Intent intent = new Intent(context, PlayerActivity.class);
+        intent.putExtra(KEY_VIDEO_FILE, file.getAbsolutePath());
+        intent.putExtra(KEY_VIDEO_TITLE, file.getName());
+        context.startActivity(intent);
+    }
+
+    public static void launchActivity(Context context, String source, String title) {
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.video);
+        mVideoView = findViewById(R.id.video_view);
+        Intent intent = getIntent();
+        if (intent.hasExtra(KEY_VIDEO_FILE)) {
+            mVideoView.playVideo(intent.getStringExtra(KEY_VIDEO_FILE));
+            Toast.makeText(this, intent.getStringExtra(KEY_VIDEO_TITLE), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mVideoView.suspend();
+    }
 }
 // https://github.com/devlucem/ZoomableVideo
 // https://medium.com/@ali.muzaffar/android-detecting-a-pinch-gesture-64a0a0ed4b41
---
+++ app/src/main/java/psycho/euphoria/v/VideoView.java
@@ -0,0 +1,189 @@
+package psycho.euphoria.v;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Matrix;
+import android.graphics.Matrix.ScaleToFit;
+import android.graphics.RectF;
+import android.net.Uri;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+public class VideoView extends FrameLayout {
+    private static final int DEFAULT_SCALING = 3;
+    private static final int IDLE = 0;
+    private static final int SCALE = 1;
+    private static final int SCROLL = 2;
+    private static final int TOLERANCE = 10;
+    private final GestureDetector mGesturesDetector;
+    private final ScaleGestureDetector mScaleDetector;
+    private final TextureVideoView mTextureVideoView;
+    private int mGestureState = IDLE;
+    Matrix m = new Matrix();
+    private float mLastScalePivotX;
+    private float mLastScalePivotY;
+    private int mWidth;
+    private RectF mVideoRect;
+    private RectF mInitialRect;
+
+    @SuppressLint("ClickableViewAccessibility")
+    public VideoView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mTextureVideoView = new TextureVideoView(getContext());
+        FrameLayout.LayoutParams layoutParams = new LayoutParams(getResources().getDisplayMetrics().widthPixels, ViewGroup.LayoutParams.MATCH_PARENT);
+        layoutParams.gravity = Gravity.CENTER;
+        addView(mTextureVideoView, layoutParams);
+        //showPartiallyDecodedImage(true);
+        GestureDetector.SimpleOnGestureListener gestureListener = new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onDoubleTap(MotionEvent e) {
+                zoomAt(e.getX(), e.getY());
+                return true;
+            }
+
+            @Override
+            public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+                if (mGestureState == SCALE) {
+                    return false;
+                }
+                mGestureState = SCROLL;
+                m.postTranslate(-distanceX, -distanceY);
+                mTextureVideoView.setAnimationMatrix(m);
+                RectF endRect = new RectF();
+                m.mapRect(endRect, mVideoRect);
+                //Log.e("B5aOx2", String.format("snapBack, %s %s", mScaleDetector.getFocusX(), endRect));
+                return true;
+            }
+
+            @Override
+            public boolean onSingleTapUp(MotionEvent ev) {
+                Log.e("B5aOx2", String.format("onSingleTapUp,%s", ev.getY()));
+                return true;
+            }
+        };
+        mGesturesDetector = new GestureDetector(getContext(), gestureListener);
+        ScaleGestureDetector.OnScaleGestureListener scaleListener = new ScaleGestureDetector.OnScaleGestureListener() {
+            @Override
+            public boolean onScale(ScaleGestureDetector detector) {
+                float scaleFactor = detector.getScaleFactor();
+                mLastScalePivotX = detector.getFocusX();
+                mLastScalePivotY = detector.getFocusY();
+                m.postScale(scaleFactor, scaleFactor, mLastScalePivotX, mLastScalePivotY);
+                mTextureVideoView.setAnimationMatrix(m);
+                return true;
+            }
+
+            @Override
+            public boolean onScaleBegin(ScaleGestureDetector detector) {
+                mGestureState = SCALE;
+                return true;
+            }
+
+            @Override
+            public void onScaleEnd(ScaleGestureDetector detector) {
+                mGestureState = IDLE;
+                snapBack();
+            }
+
+
+        };
+        mScaleDetector = new ScaleGestureDetector(getContext(), scaleListener);
+        mTextureVideoView.setOnTouchListener(this::onTouch);
+        mTextureVideoView.setOnPreparedListener(mediaPlayer -> {
+            int renderWidth = mTextureVideoView.getMeasuredWidth();
+            int renderHeight = mTextureVideoView.getMeasuredHeight();
+            int videoWidth = mediaPlayer.getVideoWidth();
+            int videoHeight = mediaPlayer.getVideoHeight();
+            mVideoRect = new RectF(0, 0, videoWidth, videoHeight);
+            mInitialRect = new RectF(0, 0, videoWidth, videoHeight);
+        });
+
+    }
+
+    public boolean onTouch(View view, MotionEvent ev) {
+        if (ev.getActionMasked() == MotionEvent.ACTION_DOWN) {
+            //endAnimation();
+            mGestureState = IDLE;
+        } else if (ev.getActionMasked() == MotionEvent.ACTION_UP && mGestureState == SCROLL) {
+            snapBack();
+            mGestureState = IDLE;
+        } else if (ev.getActionMasked() == MotionEvent.ACTION_POINTER_DOWN) {
+            mGestureState = SCALE;
+        }
+        boolean ret = mGesturesDetector.onTouchEvent(ev);
+        return mScaleDetector.onTouchEvent(ev) || ret;
+    }
+
+    public void playVideo(String path) {
+        mTextureVideoView.setVideoPath(path);
+        //mTextureVideoView.setVideoURI(Uri.parse("https://jvod.300hu.com/vod/product/9a7f27c0-e537-490a-9e24-ea2e3eaf35a6/999d6c2bf49c44d59013589923fe1be0.mp4?source=2&h265=h265/18799/04e80c7dfbc44aa78060737caa2f7fc2.mp4"));
+        mTextureVideoView.start();
+    }
+
+    public void playVideo(Uri uri) {
+        mTextureVideoView.setVideoURI(uri);
+        //mTextureVideoView.setVideoURI(Uri.parse("https://jvod.300hu.com/vod/product/9a7f27c0-e537-490a-9e24-ea2e3eaf35a6/999d6c2bf49c44d59013589923fe1be0.mp4?source=2&h265=h265/18799/04e80c7dfbc44aa78060737caa2f7fc2.mp4"));
+        mTextureVideoView.start();
+    }
+
+    private void snapBack() {
+        RectF endRect = new RectF();
+        // https://developer.android.com/reference/android/graphics/Matrix#mapRect(android.graphics.RectF,%20android.graphics.RectF)
+        m.mapRect(endRect, mVideoRect);
+        if (endRect.width() < getMeasuredWidth() && endRect.height() < getMeasuredHeight()) {
+            m = new Matrix();
+            mTextureVideoView.setAnimationMatrix(m);
+            return;
+        }
+        float offsetX = 0;
+        float offsetY = 0;
+        if (endRect.left > 0)
+            offsetX = -endRect.left;
+        else {
+            float[] values = new float[9];
+            m.getValues(values);
+            offsetX = Math.abs(values[Matrix.MTRANS_X]) - (values[Matrix.MSCALE_X] - 1) * getMeasuredWidth();
+            if (offsetX < 1)
+                return;
+        }
+        m.postTranslate(offsetX, offsetY);
+        mTextureVideoView.setAnimationMatrix(m);
+        endRect = new RectF();
+        m.mapRect(endRect, mVideoRect);
+    }
+
+    public void suspend() {
+        mTextureVideoView.suspend();
+    }
+
+    private void zoomAt(float x, float y) {
+//        Matrix startMatrix = mTextureVideoView.getMatrix();
+//        Matrix endMatrix = new Matrix();
+//        RectF currentImageRect = new RectF();
+//        startMatrix.mapRect(currentImageRect, mBitmapRect);
+//        // if (currentImageRect.width() < mWidth - TOLERANCE) {
+//        float scale = ((float) mWidth) / currentImageRect.width();
+//        Log.e("B5aOx2", String.format("zoomAt, %s", scale));
+//        endMatrix.set(startMatrix);
+        m.postScale(DEFAULT_SCALING, DEFAULT_SCALING, x, y);
+        mTextureVideoView.setAnimationMatrix(m);
+//        } else {
+//            endMatrix.setRectToRect(mBitmapRect, mInitialRect, Matrix.ScaleToFit.CENTER);
+//            mTextureVideoView.setAnimationMatrix(endMatrix);
+//        }
+    }
+//    @Override
+//    public boolean onTouchEvent(MotionEvent event) {
+//        if (event.getActionMasked() == MotionEvent.ACTION_DOWN) {
+//            Log.e("B5aOx2", String.format("[]: event.getX() = %s;\nevent.getY() = %s;\n", event.getX(), event.getY()));
+//        }
+//        return false;
+//    }
+}
